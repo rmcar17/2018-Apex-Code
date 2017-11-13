c4d76ba8ff351e1d700663b3d1788ecbcebb3fe5
Robert McArthur <23705@bbc.qld.edu.au>
2017-11-14 09:32:11 +1000

    Setup a simple structure for teh MotorController code

---
+++ lib/MotorController/MotorController.cpp
@@ -0,0 +1,11 @@
+#include "MotorController.h"
+
+MotorController::MotorController(){
+
+}
+
+void MotorController::MoveDirection(int angle, int speed){
+  int a;
+  int b = 1;
+  
+}
--- lib/MotorController/MotorController.h
+++ lib/MotorController/MotorController.h
@@ -1,1 +1,9 @@
 #include <Motor.h>
+#include <Defines.h>
+
+class MotorController {
+  public:
+    MotorController();
+
+    void MoveDirection(int angle, int speed);
+}

19dac0daa95d675bc21cc3c3751ec010b3d6bbfd
Robert McArthur <23705@bbc.qld.edu.au>
2017-11-14 09:42:05 +1000

    Created functions which convert radians to degrees and vice versa.

---
+++ lib/_Common/Common.cpp
@@ -0,0 +1,9 @@
+#include "Common.h"
+
+double toDegrees(double rad){
+  return rad*TO_DEGREES;
+}
+
+double toRadians(double deg){
+  return deg*TO_RADIANS;
+}
---
+++ lib/_Common/Common.h
@@ -0,0 +1,11 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#define TO_RADIANS 0.01745329251994329576923690768489
+#define TO_DEGREES 57.29577951308232087679815481410517
+
+
+double toDegrees(double rad);
+double toRadians(double deg);
+
+#endif

646c8a506671fe10a973317b25af429b68ea1624
Robert McArthur <23705@bbc.qld.edu.au>
2017-11-14 10:24:43 +1000

    Created motor movement calculations

--- lib/MotorController/MotorController.cpp
+++ lib/MotorController/MotorController.cpp
@@ -4,8 +4,35 @@ MotorController::MotorController(){
 
 }
 
-void MotorController::MoveDirection(int angle, int speed){
-  int a;
-  int b = 1;
-  
+void MotorController::MoveDirection(int angle, int speed, int rotation){
+  double multiplier;
+
+  double newAngle = toRadians(mod(45-angle,360));
+
+  int b = angle > 135 && angle < 315 ? -speed : speed;
+  int a = round(b * tan(newAngle));
+
+  if(abs(a)>speed){
+    multiplier = abs(speed/a);
+    b = round(b*multiplier);
+    a = round(a*multiplier);
+  }
+
+  int frontRightSpeed = a + rotation;
+  int backRightSpeed = -b + rotation;
+  int backLeftSpeed = -a + rotation;
+  int frontLeftSpeed = b + rotation;
+
+
+
+  multiplier = speed/max(abs(frontRightSpeed),max(abs(backRightSpeed),max(abs(backLeftSpeed),abs(frontLeftSpeed))));
+
+  frontRightSpeed = round(frontRightSpeed*multiplier);
+  backRightSpeed = round(backRightSpeed*multiplier);
+  backLeftSpeed = round(backLeftSpeed*multiplier);
+  frontLeftSpeed = round(frontLeftSpeed*multiplier);
+
+  /*
+  Move Motors their various speeds
+  */
 }
--- lib/MotorController/MotorController.h
+++ lib/MotorController/MotorController.h
@@ -1,9 +1,15 @@
+#ifndef MOTORCONTROLLER_H_
+#define MOTORCONTROLLER_H_
+
 #include <Motor.h>
 #include <Defines.h>
+#include <Common.h>
 
 class MotorController {
   public:
     MotorController();
 
-    void MoveDirection(int angle, int speed);
-}
+    void MoveDirection(int angle, int speed, int rotation);
+};
+
+#endif

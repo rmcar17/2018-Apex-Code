eab78e9bf2e9e63aeef5ede1627ab34b00aa8bf1
Robert McArthur <23705@bbc.qld.edu.au>
2017-11-14 10:24:43 +1000

    Created motor movement calculations

--- lib/MotorController/MotorController.h
+++ lib/MotorController/MotorController.h
@@ -1,9 +1,10 @@
 #include <Motor.h>
 #include <Defines.h>
+#include <Common.h>
 
 class MotorController {
   public:
     MotorController();
 
-    void MoveDirection(int angle, int speed);
-}
+    void MoveDirection(int angle, int speed, int rotation);
+};

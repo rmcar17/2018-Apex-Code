27a7ec262f34bbfda8bda04adb5cf762db46042a
rmcar17 <robertnmcarthur@hotmail.com>
2017-11-16 14:08:38 +1000

    Created testing code for motor spinning

---
+++ lib/_Pins/Pins.h
@@ -0,0 +1,8 @@
+#ifndef PINS_H_
+#define PINS_H_
+
+#define MOTOR1_PWM 7
+#define MOTOR1_IN1 6
+#define MOTOR1_IN2 5
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,16 @@
 #include <Pins.h>
 
 #include <MotorController.h>
+#include <Motor.h>
+
+Motor motor1;
 
 void setup() {
+  motor1 = Motor(MOTOR1_PWM,MOTOR1_IN1,MOTOR1_IN2);
     // put your setup code here, to run once:
 }
 
 void loop() {
+  motor1.move(255);
     // put your main code here, to run repeatedly:
 }

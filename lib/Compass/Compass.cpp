93866365369d23f4238120f182264f5d289a26ac
rmcar17 <robertnmcarthur@hotmail.com>
2017-11-16 16:20:40 +1000

    Removed useless functions

--- lib/Compass/Compass.cpp
+++ lib/Compass/Compass.cpp
@@ -10,19 +10,6 @@ void Compass::CompassSetup() {
 
     previousTime = micros();
 };
-
-Vector3D Compass::readAccelerometer() {
-    uint8_t buffer[14];
-    I2Cread(MPU9250_ADDRESS, 0x3B, 14, buffer);
-
-    int16_t ax = -(buffer[0] << 8 | buffer[1]);
-    int16_t ay = -(buffer[2] << 8 | buffer[5]);
-    int16_t az = buffer[4] << 8 | buffer[5];
-
-    Vector3D returnVector = {convertRawAcceleration(ax), convertRawAcceleration(ay), convertRawAcceleration(az)};
-    return returnVector;
-}
-
 Vector3D Compass::readGyroscope() {
     uint8_t buffer[14];
     I2Cread(MPU9250_ADDRESS, 0x3B, 14, buffer);
@@ -32,23 +19,6 @@ Vector3D Compass::readGyroscope() {
     int16_t gz = buffer[12] << 8 | buffer[13];
 
     Vector3D returnVector = {convertRawGyro(gx), convertRawGyro(gy), convertRawGyro(gz)};
-    return returnVector;
-}
-
-Vector3D Compass::readMagnetometer() {
-    uint8_t status1;
-    do {
-        I2Cread(MAG_ADDRESS, 0x02, 1, &status1);
-    } while (!(status1 & 0x01));
-
-    uint8_t mag[7];
-    I2Cread(MAG_ADDRESS, 0x03, 7, mag);
-
-    int16_t mx = -(mag[3] << 8 | mag[2]);
-    int16_t my = -(mag[1] << 8 | mag[0]);
-    int16_t mz = -(mag[5] << 8 | mag[4]);
-
-    Vector3D returnVector = {(double) mx, (double) my, (double) mz};
     return returnVector;
 }
 

60df80d560293ef4568e1a90385ac1dd68679661
rmcar17 <robertnmcarthur@hotmail.com>
2017-11-16 14:19:24 +1000

    Confirmed FR Pins

--- lib/_Pins/Pins.h
+++ lib/_Pins/Pins.h
@@ -4,12 +4,16 @@
 
 //MOTORS
 
-#define MOTOR1_PWM 4
-#define MOTOR1_IN1 3
-#define MOTOR1_IN2 2
+#define MOTORFR_PWM 4
+#define MOTORFR_IN1 3
+#define MOTORFR_IN2 2
 
 #define MOTORBR_PWM 7
 #define MOTORBR_IN1 6
 #define MOTORBR_IN2 5
+
+#define MOTOR1_PWM 11
+#define MOTOR1_IN1 12
+#define MOTOR1_IN2 13
 
 #endif

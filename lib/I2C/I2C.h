20c5aec48fa7a1415fe16358f8e3f29ab276d23e
rmcar17 <robertnmcarthur@hotmail.com>
2017-11-16 16:14:46 +1000

    Copied in compass code

---
+++ lib/I2C/I2C.h
@@ -0,0 +1,9 @@
+#ifndef I2C_H_
+#define I2C_H_
+
+#include <Wire.h>
+
+void I2Cread(uint8_t address, uint8_t registerAddress, uint8_t nBytes, uint8_t *data);
+void I2CwriteByte(uint8_t address, uint8_t registerAddress, uint8_t data);
+
+#endif

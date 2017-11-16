f45cbee1865761f8a4f11155d91cf3e01103eec8
rmcar17 <robertnmcarthur@hotmail.com>
2017-11-16 14:25:47 +1000

    Confirmed all motors

--- lib/_Pins/Pins.h
+++ lib/_Pins/Pins.h
@@ -16,8 +16,8 @@
 #define MOTORBL_IN1 8
 #define MOTORBL_IN2 9
 
-#define MOTOR1_PWM 13
-#define MOTOR1_IN1 12
-#define MOTOR1_IN2 11
+#define MOTORBR_PWM 13
+#define MOTORBR_IN1 12
+#define MOTORBR_IN2 11
 
 #endif

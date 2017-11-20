ba5d01c5c99a460b161aa2af52baec24d9e0c7af
rmcar17 <robertnmcarthur@hotmail.com>
2017-11-20 15:55:15 +1000

    Added minimalistic compass correcting

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,33 @@
 
 #include <Compass.h>
 
+Compass comp;
+
 MotorController motors;
 
+int compassCorrect(){
+  Serial.println(comp.heading);
+  int correction = 360-comp.heading;
+
+  correction = correction<180 ? -correction:360-correction;
+  correction *= 5;
+
+  return correction;
+}
+
 void setup() {
+  Serial.begin(9600);
+
+  Wire.begin();
+  comp.compassSetup();
+
   motors.motorSetup();
+
+  comp.calibrate();
 }
 
 void loop() {
-  motors.moveDirection(315, 255, 0);
+  comp.updateGyro();
+
+  motors.moveDirection(-1, 255, compassCorrect());
 }

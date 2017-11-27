70e1080ce32b94b219a496bd51925897ef9a0a41
rmcar17 <robertnmcarthur@hotmail.com>
2017-11-27 15:34:18 +1000

    Added basic debug options

---
+++ lib/_Debug/Debug.h
@@ -0,0 +1,10 @@
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+#define DEBUG_MOTOR 0
+#define DEBUG_COMPASS 0
+#define DEBUG_PIXY 0
+
+const bool DEBUG_ANY = DEBUG_MOTOR || DEBUG_COMPASS || DEBUG_PIXY;
+
+#endif

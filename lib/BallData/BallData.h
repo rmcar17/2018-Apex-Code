b408b861f9ddb4219913f38c9d963329fd8e2c1e
rmcar17 <robertnmcarthur@hotmail.com>
2018-01-29 16:00:35 +1000

    Added BallData and MoveData

---
+++ lib/BallData/BallData.h
@@ -0,0 +1,9 @@
+#ifndef BALLDATA_H_
+#define BALLDATA_H_
+
+struct BallData {
+  int angle;
+  int strength;
+};
+
+#endif
---
+++ lib/MoveData/MoveData.h
@@ -0,0 +1,10 @@
+#ifndef MOVEDATA_H_
+#define MOVEDATA_H_
+
+struct MoveData {
+  int angle;
+  int speed;
+  int rotation;
+};
+
+#endif

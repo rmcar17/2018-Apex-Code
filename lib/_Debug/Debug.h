8a13f8ed4928f2e4e31e7739e473def9c5ca7da4
Robert McArthur <23705@bbc.qld.edu.au>
2018-02-01 21:14:41 +1000

    Debug Compass

--- lib/_Debug/Debug.h
+++ lib/_Debug/Debug.h
@@ -2,9 +2,9 @@
 #define DEBUG_H
 
 #define DEBUG_MOTOR 0
-#define DEBUG_COMPASS 0
+#define DEBUG_COMPASS 1
 #define DEBUG_PIXY 0
-#define DEBUG_TSOPS 1
+#define DEBUG_TSOPS 0
 
 const bool DEBUG_ANY = DEBUG_MOTOR || DEBUG_COMPASS || DEBUG_PIXY;
 

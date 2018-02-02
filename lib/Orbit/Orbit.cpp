8834ea17675e2480a7e992d01acaefac0d023b3d
rmcar17 <robertnmcarthur@hotmail.com>
2018-02-02 12:48:38 +1000

    Added getCloseOrbit

--- lib/Orbit/Orbit.cpp
+++ lib/Orbit/Orbit.cpp
@@ -7,10 +7,25 @@ Orbit::Orbit(){
 void Orbit::calculateMoveData(BallData ballData){
   distance = ballData.distance;
   direction = ballData.angle;
+
+  if(distance > CLOSE_ORBIT){
+    getCloseOrbit();
+  }
+  else if(distance < FAR_ORBIT){
+
+  }
+  else{
+
+  }
 }
 
 MoveData Orbit::getMoveData(){
   return Movement;
+}
+
+void Orbit::getCloseOrbit(){
+  Movement.speed = SPEED;
+  Movement.angle = direction < 180 ? direction + 90 : direction - 90;
 }
 
 void Orbit::resetMoveData(int dir = -1, int speed = 0, int rot = 0){

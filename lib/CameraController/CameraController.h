846d4caa81fdda3ec4e710dca3154f0420421809
Robert McArthur <23705@bbc.qld.edu.au>
2018-03-05 16:35:42 +1000

    Added setting methods

--- lib/CameraController/CameraController.h
+++ lib/CameraController/CameraController.h
@@ -1,5 +1,16 @@
 #ifndef CAMERACONTROLLER_H
 #define CAMERACONTROLLER_H
 
+#include <EntityData.h>
+
+class CameraController {
+  public:
+    CameraController();
+
+    void setAttackGoal(int x, int y, int pixels);
+    void setDefendGoal(int x, int y, int pixels);
+    void setBall(int x, int y, int pixels);
+
+};
 
 #endif

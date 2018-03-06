694b3e376112330a2fa404cba8178b33df1f30b9
Robert McArthur <23705@bbc.qld.edu.au>
2018-03-06 18:33:39 +1000

    Added empty methods

--- lib/CameraController/CameraController.cpp
+++ lib/CameraController/CameraController.cpp
@@ -1,1 +1,41 @@
 #include "CameraController.h"
+
+CameraController::CameraController(){
+
+}
+
+void CameraController::setAttackGoal(int x, int y, int pixels){
+
+}
+
+void CameraController::setDefendGoal(int x, int y, int pixels){
+
+}
+
+void CameraController::setBall(int x, int y, int pixels){
+
+}
+
+int CameraController::calculateAngle(int x, int y){
+
+}
+
+int CameraController::calculateGoalDistance(int pixels){
+
+}
+
+int CameraController::calculateBallDistance(int pixels){
+
+}
+
+EntityData CameraController::getAttackGoal(){
+
+}
+
+EntityData CameraController::getDefendGoal(){
+
+}
+
+EntityData CameraController::getBall(){
+  
+}

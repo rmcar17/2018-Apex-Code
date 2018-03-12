1091fa34007ece4d5a8c68c6f3e83850936eec04
Robert McArthur <23705@bbc.qld.edu.au>
2018-03-12 17:12:37 +1000

    c

--- lib/CameraController/CameraController.cpp
+++ lib/CameraController/CameraController.cpp
@@ -5,15 +5,16 @@ CameraController::CameraController(){
 }
 
 void CameraController::setAttackGoal(int x, int y, int pixels){
-
+  //Image aGoal = Camera.getAttackGoal();
+  //attackGoal.angle = calculateAngle(aGoal.x, aGoal.y);
 }
 
 void CameraController::setDefendGoal(int x, int y, int pixels){
-
+  //Image dGoal = Camera.getAttackGoal();
 }
 
 void CameraController::setBall(int x, int y, int pixels){
-
+  //Image ballImage = Camera.getBall();
 }
 
 int CameraController::calculateAngle(int x, int y){
@@ -37,5 +38,5 @@ EntityData CameraController::getDefendGoal(){
 }
 
 EntityData CameraController::getBall(){
-  
+
 }

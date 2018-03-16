99971d98792ce0b17b2e4b53586e25eaac5a9113
rmcar17 <robertnmcarthur@hotmail.com>
2018-03-16 10:03:06 +1000

    Added temporary return statements to methods which were causing errors.

--- lib/CameraController/CameraController.cpp
+++ lib/CameraController/CameraController.cpp
@@ -10,7 +10,7 @@ void CameraController::setAttackGoal(Image aGoal){
 }
 
 void CameraController::setDefendGoal(Image dGoal){
-  defendGoal.angle = calculateAngle(aGoal.x, aGoal.y);
+  defendGoal.angle = calculateAngle(dGoal.x, dGoal.y);
   //defendGoal.distance = calculateGoalDistance(dGoal.pixels);
 }
 
@@ -20,25 +20,25 @@ void CameraController::setBall(Image ballImage){
 }
 
 int CameraController::calculateAngle(int x, int y){
-
+  return 0;
 }
 
 int CameraController::calculateGoalDistance(int pixels){
-
+  return 0;
 }
 
 int CameraController::calculateBallDistance(int pixels){
-
+  return 0;
 }
 
 EntityData CameraController::getAttackGoal(){
-
+  return {0,0,0};
 }
 
 EntityData CameraController::getDefendGoal(){
-
+  return {0,0,0};
 }
 
 EntityData CameraController::getBall(){
-
+  return {0,0,0};
 }

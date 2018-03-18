f91b34fe16f444573d10720df3bdb5bd6ef98eb6
Robert McArthur <23705@bbc.qld.edu.au>
2018-03-18 11:29:43 +1000

    Added get methods

--- lib/Camera/Camera.cpp
+++ lib/Camera/Camera.cpp
@@ -11,3 +11,15 @@ void Camera::setup(){
 void Camera::update(){
 
 }
+
+Image Camera::getAttackGoal(){
+  return attackGoal;
+}
+
+Image Camera::getDefendGoal(){
+  return defendGoal;
+}
+
+Image Camera::getBall(){
+  return ball;
+}

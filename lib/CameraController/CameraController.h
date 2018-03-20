e0f8f8545cb3e0ae7cde41b827d8f37cd5716406
Robert McArthur <23705@bbc.qld.edu.au>
2018-03-20 14:46:33 +1000

    Merged distance calculation methods into calculateDistance

--- lib/CameraController/CameraController.h
+++ lib/CameraController/CameraController.h
@@ -23,8 +23,7 @@ class CameraController {
     void calculateBall(Image ballImage);
 
     int calculateAngle(int x, int y);
-    double calculateGoalDistance(int pixels);
-    double calculateBallDistance(int pixels);
+    double calculateDistance(int x, int y);
 
     // Camera camera;
 

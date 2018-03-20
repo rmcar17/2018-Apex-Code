cfc8fdc82d01cf95c082f4a2f6427bb18a4c4f11
Robert McArthur <23705@bbc.qld.edu.au>
2018-03-20 13:58:40 +1000

    Removed excess commenting

--- Camera/EntityDetection.py
+++ Camera/EntityDetection.py
@@ -1,16 +1,10 @@
-# Multi Color Blob Tracking Example
-#
-# This example shows off multi color blob tracking using the OpenMV Cam.
-
 import sensor, image, time
 
 # Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
-# The below thresholds track in general red/green things. You may wish to tune them...
 thresholds = [(30, 100, 15, 127, 15, 127), # generic_red_thresholds
               (30, 100, -64, -8, -32, 32), # generic_green_thresholds
               (0, 15, 0, 40, -80, -20)] # generic_blue_thresholds
-# You may pass up to 16 thresholds above. However, it's not really possible to segment any
-# scene with 16 thresholds before color thresholds start to overlap heavily.
+# MAX 16 thresholds
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
@@ -19,10 +13,6 @@ sensor.skip_frames(time = 2000)
 sensor.set_auto_gain(False) # must be turned off for color tracking
 sensor.set_auto_whitebal(False) # must be turned off for color tracking
 clock = time.clock()
-
-# Only blobs that with more pixels than "pixel_threshold" and more area than "area_threshold" are
-# returned by "find_blobs" below. Change "pixels_threshold" and "area_threshold" if you change the
-# camera resolution. Don't set "merge=True" becuase that will merge blobs which we don't want here.
 
 while(True):
     clock.tick()

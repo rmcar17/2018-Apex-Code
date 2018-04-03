1756c08bba9b951b340d95b0f1a0d44093c9ebc5
Robert McArthur <23705@bbc.qld.edu.au>
2018-04-03 17:11:37 +1000

    Calibrated yellow goal

--- Camera/EntityDetection.py
+++ Camera/EntityDetection.py
@@ -3,6 +3,7 @@ import sensor, image, time
 # (L Min, L Max, A Min, A Max, B Min, B Max)
 ball = [(38, 55, 32, 66, 17, 50)]
 blue_goal = [(29, 35,-6,3,-23,-11)]
+yellow_goal = [(49,56,-3,9,11,30)]
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
@@ -15,7 +16,7 @@ clock = time.clock()
 while(True):
     clock.tick()
     img = sensor.snapshot()
-    for blob in img.find_blobs(blue_goal):
+    for blob in img.find_blobs(yellow_goal):
         img.draw_rectangle(blob.rect())
         img.draw_cross(blob.cx(), blob.cy())
     print(clock.fps())

b005f0cc4bcb2d681eb9c93337ef6d194a5faab2
Robert McArthur <23705@bbc.qld.edu.au>
2018-04-03 16:39:17 +1000

    Removed excessive commenting

--- Camera/EntityDetection.py
+++ Camera/EntityDetection.py
@@ -1,17 +1,16 @@
 import sensor, image, time
 
-# Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
-thresholds = [(30, 100, 15, 127, 15, 127), # generic_red_thresholds
-              (30, 100, -64, -8, -32, 32), # generic_green_thresholds
-              (0, 15, 0, 40, -80, -20)] # generic_blue_thresholds
-# MAX 16 thresholds
+# (L Min, L Max, A Min, A Max, B Min, B Max)
+thresholds = [(30, 100, 15, 127, 15, 127),
+              (30, 100, -64, -8, -32, 32),
+              (0, 15, 0, 40, -80, -20)]
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
-sensor.set_auto_gain(False) # must be turned off for color tracking
-sensor.set_auto_whitebal(False) # must be turned off for color tracking
+sensor.set_auto_gain(False)
+sensor.set_auto_whitebal(False)
 clock = time.clock()
 
 while(True):

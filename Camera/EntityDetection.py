d5b7c67c28ba1325020ada94950f2281132c91ea
Robert McArthur <23705@bbc.qld.edu.au>
2018-04-04 11:33:40 +1000

    Serial communication with one variable

--- Camera/EntityDetection.py
+++ Camera/EntityDetection.py
@@ -1,9 +1,12 @@
 import sensor, image, time
+from pyb import UART
 
 # (L Min, L Max, A Min, A Max, B Min, B Max)
 ball = [(38, 55, 32, 66, 17, 50)]
 blue_goal = [(29, 35,-6,3,-23,-11)]
 yellow_goal = [(49,56,-3,9,11,30)]
+
+uart = UART(3, 9600, timeout_char = 1000)
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
@@ -19,4 +22,5 @@ while(True):
     for blob in img.find_blobs(yellow_goal):
         img.draw_rectangle(blob.rect())
         img.draw_cross(blob.cx(), blob.cy())
+    uart.writechar(100)
     print(clock.fps())

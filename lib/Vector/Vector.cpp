cbbb5fe9b64d8f130a850062b66e8f4030347e7d
Robert McArthur <23705@bbc.qld.edu.au>
2018-04-05 16:08:26 +1000

    Implemented calculation methods

--- lib/Vector/Vector.cpp
+++ lib/Vector/Vector.cpp
@@ -8,15 +8,31 @@ void Vector::setStandard(double _i, double _j){
   i = _i;
   j = _j;
 
-  mod = sqrt(_i * _i + _j * _j);
-  arg = atan2(_j, _i)
+  mod = calcMod(_i, _j);
+  arg = calcArg(_i, _j);
 }
 
 void Vector::setPolar(double _mod, double _arg){
   // Convert to standard and set i and j
-  i = _mod * cos(_arg);
-  j = _mod * sin(_arg);
+  i = calcI(_mod, _arg);
+  j = calcJ(_mod, _arg);
 
   mod = _mod;
   arg = _arg;
+}
+
+double Vector::calcI(double _mod, double _arg){
+  return _mod * cos(_arg);
+}
+
+double Vector::calcJ(double _mod, double _arg){
+  return _mod * sin(_arg);
+}
+
+double Vector::calcMod(double _i, double _j){
+  return sqrt(_i * _i + _j * _j);
+}
+
+double Vector::calcArg(double _i, double _j){
+  return atan2(_j, _i)
 }

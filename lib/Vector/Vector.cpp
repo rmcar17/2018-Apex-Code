56d06f1f468115c066865c74316a78cd63581aca
Robert McArthur <23705@bbc.qld.edu.au>
2018-04-05 16:02:20 +1000

    setStandard now also sets mod and arg

--- lib/Vector/Vector.cpp
+++ lib/Vector/Vector.cpp
@@ -7,6 +7,9 @@ Vector::Vector(){
 void Vector::setStandard(double _i, double _j){
   i = _i;
   j = _j;
+
+  mod = sqrt(_i * _i + _j * _j);
+  arg = atan2(_j, _i)
 }
 
 void Vector::setPolar(double _mod, double _arg){

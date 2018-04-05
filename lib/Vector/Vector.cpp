e264920f0931f278c5ecb8e1b23fa0d0c2ca4c50
Robert McArthur <23705@bbc.qld.edu.au>
2018-04-05 15:58:22 +1000

    Renamed angle to argument

--- lib/Vector/Vector.cpp
+++ lib/Vector/Vector.cpp
@@ -9,8 +9,8 @@ void Vector::setStandard(double _i, double _j){
   j = _j;
 }
 
-void Vector::setPolar(double magnitude, double angle){
+void Vector::setPolar(double magnitude, double argument){
   // Convert to standard and set i and j
-  i = magnitude * cos(angle);
-  j = magnitude * sin(angle);
+  i = magnitude * cos(argument);
+  j = magnitude * sin(argument);
 }
--- lib/Vector/Vector.h
+++ lib/Vector/Vector.h
@@ -8,13 +8,13 @@ class Vector {
     Vector();
 
     void setStandard(double _i, double _j);
-    void setPolar(double magnitude, double angle);
+    void setPolar(double magnitude, double argument);
 
     Vector operator+(Vector vector2);
     Vector operator-(Vector vector2);
 
     double i, j;
-    double magnitude, angle;
+    double magnitude, argument;
 };
 
 #endif

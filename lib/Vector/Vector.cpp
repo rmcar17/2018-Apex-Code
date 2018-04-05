d7fbfe6dcb3303b72e4d43fde66444665da78d7f
Robert McArthur <23705@bbc.qld.edu.au>
2018-04-05 15:54:11 +1000

    Added option to set a vector from polar values

--- lib/Vector/Vector.cpp
+++ lib/Vector/Vector.cpp
@@ -8,3 +8,9 @@ void Vector::setStandard(double _i, double _j){
   i = _i;
   j = _j;
 }
+
+void Vector::setPolar(double magnitude, double angle){
+  // Convert to standard and set i and j
+  i = magnitude * cos(angle);
+  j = magnitude * sin(angle);
+}

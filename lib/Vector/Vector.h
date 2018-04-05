209049b50cd2db862c29850ac4ce4b7286ab9ae6
Robert McArthur <23705@bbc.qld.edu.au>
2018-04-05 15:50:34 +1000

    Changed order of initialising vectors to display priority

--- lib/Vector/Vector.h
+++ lib/Vector/Vector.h
@@ -5,8 +5,8 @@ class Vector {
   public:
     Vector();
 
-    void setPolar(double magnitude, double angle);
     void setStandard(double _i, double _j);
+    void setPolar(double magnitude, double angle);
 
     Vector operator+(Vector vector2);
     Vector operator-(Vector vector2);

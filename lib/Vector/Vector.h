2be4e687db333f26c2043ad999dbfff7dfc6b4e0
Robert McArthur <23705@bbc.qld.edu.au>
2018-05-02 09:00:25 +1000

    Defined comparative operators for vectors

--- lib/Vector/Vector.h
+++ lib/Vector/Vector.h
@@ -8,18 +8,25 @@ class Vector {
     Vector(double val1, double val2, bool polar = true);
 
     void setStandard(double _i, double _j);
-    void setPolar(double _mod, double _arg);
+    void setPolar(double _mag, double _arg);
 
     Vector operator+(Vector vector2);
     Vector operator-(Vector vector2);
 
-    double i, j;
-    double mod, arg;
-  private:
-    double calcI(double _mod, double _arg);
-    double calcJ(double _mod, double _arg);
+    bool operator==(Vector vector2);
+    bool operator!=(Vector vector2);
+    bool operator<(Vector vector2);
+    bool operator<=(Vector vector2);
+    bool operator>(Vector vector2);
+    bool operator>=(Vector vector2);
 
-    double calcMod(double _i, double _j);
+    double i, j;
+    double mag, arg;
+  private:
+    double calcI(double _mag, double _arg);
+    double calcJ(double _mag, double _arg);
+
+    double calcmag(double _i, double _j);
     double calcArg(double _i, double _j);
 };
 

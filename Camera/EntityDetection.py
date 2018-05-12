64a0cdbc78caa7209ab6897e5ff56d9905748896
Robert McArthur <23705@bbc.qld.edu.au>
2018-05-12 17:11:48 +1000

    Calibrated camera for home

--- Camera/EntityDetection.py
+++ Camera/EntityDetection.py
@@ -2,7 +2,7 @@ import sensor, image, time
 from pyb import UART, LED
 
 # (L Min, L Max, A Min, A Max, B Min, B Max)
-ball = [(50, 69, 35, 70, 5, 48)]
+ball = [(33, 50, 7, 42, 31, 55)]
 blueGoal = [(41, 50,-8,-4,-22,-15)]
 yellowGoal = [(61,70,-3,10,15,32)]
 
@@ -11,11 +11,17 @@ uart = UART(3, 9600, timeout_char = 1000)
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
-sensor.skip_frames(time = 1000)
+sensor.skip_frames(time = 2000)
 sensor.set_auto_gain(False)
 sensor.set_auto_whitebal(False)
-clock = time.clock()
-#LED(1).on()
+
+LED(1).on()
+time.sleep(200)
+LED(1).off()
+#sensor.set_saturation(3)
+#sensor.set_brightness(1)
+
+#clock = time.clock()
 def largestBlob(lBlob):
     if not lBlob:
         return None
@@ -30,9 +36,10 @@ while(True):
     sendBuffer = [1,0,0,0,0,0,0,0,0,0]
 
     img = sensor.snapshot()
-    ballBlob = largestBlob(img.find_blobs(ball))
-    blueBlob = largestBlob(img.find_blobs(blueGoal))
-    yellowBlob = largestBlob(img.find_blobs(yellowGoal))
+
+    ballBlob = largestBlob(img.find_blobs(ball,roi=(27,0,252,240),x_stride=3,y_stride=3))
+    blueBlob = largestBlob(img.find_blobs(blueGoal,roi=(27,0,252,240),x_stride=8,y_stride=4))
+    yellowBlob = largestBlob(img.find_blobs(yellowGoal,roi=(27,0,252,240),x_stride=8,y_stride=4))
 
     if ballBlob:
         #img.draw_cross(ballBlob.cx(), ballBlob.cy())
@@ -58,17 +65,11 @@ while(True):
     for i in range(1,len(sendBuffer)):
         if sendBuffer[i] == 1:
             sendBuffer[i] = 2
-    #string = ""
-    #LED(1).on()
     for i in sendBuffer:
         try:
             if i == 0:
                 i += 1
             uart.writechar(i)
-            #print(i)
         except Exception as E:
             print(E)
-        #string += " "+str(i)
-    #print(string)
-    #LED(1).off()
     #print(clock.fps())

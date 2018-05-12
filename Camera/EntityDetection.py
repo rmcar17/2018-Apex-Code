f76544fad20199fbad9e4058745bd373b71fd13b
Robert McArthur <23705@bbc.qld.edu.au>
2018-05-12 21:52:51 +1000

    Recalibrated camera, LED flashes when turned on

--- Camera/EntityDetection.py
+++ Camera/EntityDetection.py
@@ -2,7 +2,7 @@ import sensor, image, time
 from pyb import UART, LED
 
 # (L Min, L Max, A Min, A Max, B Min, B Max)
-ball = [(33, 50, 7, 42, 31, 55)]
+ball = [(33, 50, 10, 42, 31, 55)]
 blueGoal = [(41, 50,-8,-4,-22,-15)]
 yellowGoal = [(61,70,-3,10,15,32)]
 
@@ -42,7 +42,8 @@ while(True):
     yellowBlob = largestBlob(img.find_blobs(yellowGoal,roi=(27,0,252,240),x_stride=8,y_stride=4))
 
     if ballBlob:
-        #img.draw_cross(ballBlob.cx(), ballBlob.cy())
+        img.draw_cross(ballBlob.cx(), ballBlob.cy())
+        print((((ballBlob.cx()-160)**2+(ballBlob.cy()-120)**2)**0.5)*50/237)
         if ballBlob.cx() >= 255:
             sendBuffer[1] = 255
         sendBuffer[2] = ballBlob.cx() % 255
@@ -67,8 +68,6 @@ while(True):
             sendBuffer[i] = 2
     for i in sendBuffer:
         try:
-            if i == 0:
-                i += 1
             uart.writechar(i)
         except Exception as E:
             print(E)

129c189db45691eb4f3b6b634ebc5911f72c304b
Robert McArthur <23705@bbc.qld.edu.au>
2018-08-12 19:56:12 +1000

    States orbit used in finals

--- lib/Orbit/Orbit.cpp
+++ lib/Orbit/Orbit.cpp
@@ -192,7 +192,6 @@ void Orbit::calcSmallOrbit(){
   movement.speed = MAX_SPEED;
   // movement.angle = (ball.arg < 180 ? ball.arg*ANGLE_TIGHTENER_RIGHT-SMALL_OFFSET_RIGHT : 360-(360-ball.arg)*ANGLE_TIGHTENER_LEFT-SMALL_OFFSET_LEFT);
   movement.angle = (ball.arg < 180 ? ball.arg : 360-(360-ball.arg));
-  movement.angle += attackGoal.arg;
 }
 
 void Orbit::calcBigOrbit(){

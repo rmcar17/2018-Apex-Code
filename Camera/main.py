c722356eebfbd24f2e85c495ec5244dbd6faa7e7
Coolkidgoodking <24618@bbc.qld.edu.au>
2018-09-13 14:57:56 +1000

    new calibration

--- Camera/main.py
+++ Camera/main.py
@@ -8,9 +8,9 @@ robot = 1
 
 if robot == 1:
     camThresholds = [
-    ((26, 63, 42, 90, 38, 81),), #Ball
+    ((38, 65, 34, 86, 14, 83),), #Ball
     ((27, 41, -23, 5, -38, -16),), #Blue Goal
-    ((44, 84, -2, 38, 29, 78),)]   #Yellow Goal
+    ((55, 88, -17, 56, 32, 87),)]   #Yellow Goal
 else:
     camThresholds = [
     ((41, 65, 34, 80, 22, 67),),
@@ -41,17 +41,17 @@ class Reader:
     def update(self):
         img = sensor.snapshot()
 
-        ballBlob = self.getXY(largestBlob(img.find_blobs(self.thresholds[0],x_stride=4,y_stride=4,pixels_threshold=20)))
-        blueBlob = self.getXY(largestBlob(img.find_blobs(self.thresholds[1],x_stride=5,y_stride=5,merge=True,margin=34,pixels_threshold=100)))
-        yellowBlob = self.getXY(largestBlob(img.find_blobs(self.thresholds[2],x_stride=5,y_stride=5,merge=True,margin=34,pixels_threshold=50)))
+        ballBlob = self.getXY(largestBlob(img.find_blobs(self.thresholds[0],x_stride=2,y_stride=2,pixels_threshold=20)))
+        blueBlob = self.getXY(largestBlob(img.find_blobs(self.thresholds[1],x_stride=15,y_stride=8,merge=True,margin=34,pixels_threshold=100)))
+        yellowBlob = self.getXY(largestBlob(img.find_blobs(self.thresholds[2],x_stride=15,y_stride=8,merge=True,margin=34,pixels_threshold=50)))
 
         if self.debug:
             if ballBlob != (255, 255):
-                img.draw_line((100, 100, ballBlob[0], ballBlob[1]),color=(255,92,0),thickness=2)
+                img.draw_line((100, 115, ballBlob[0], ballBlob[1]),color=(255,92,0),thickness=2)
             if blueBlob != (255, 255):
-                img.draw_line((100, 100, blueBlob[0], blueBlob[1]),color=(0,75,255),thickness=2)
+                img.draw_line((100, 115, blueBlob[0], blueBlob[1]),color=(0,75,255),thickness=2)
             if yellowBlob != (255, 255):
-                img.draw_line((100, 100, yellowBlob[0], yellowBlob[1]),color=(255,255,0),thickness=2)
+                img.draw_line((100, 115, yellowBlob[0], yellowBlob[1]),color=(255,255,0),thickness=2)
 
         self.data = (ballBlob[0],ballBlob[1],blueBlob[0],blueBlob[1],yellowBlob[0],yellowBlob[1])
 
@@ -68,7 +68,7 @@ class Reader:
         sensor.skip_frames(time = 1000)
         sensor.set_auto_gain(False)
         sensor.set_auto_whitebal(False)
-        sensor.set_windowing((60,20,200,200))
+        sensor.set_windowing((60,5,200,230))
 
         sensor.set_saturation(3)
         sensor.set_brightness(-2)

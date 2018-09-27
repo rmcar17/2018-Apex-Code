357c0de53f8c9b7827e9560c7fffb1e339208d12
Robert McArthur <23705@bbc.qld.edu.au>
2018-09-27 11:52:57 +1000

    Kicker works in isolation

--- master/src/main.cpp
+++ master/src/main.cpp
@@ -1,118 +1,22 @@
 #include <Arduino.h>
-#include <MotorController.h>
-#include <Motor.h>
-#include <Compass.h>
-#include <LightSensor.h>
-#include <LightSensorController.h>
-#include <Camera.h>
-#include <CameraController.h>
-#include <Kicker.h>
-#include <LightGate.h>
-#include <RoleController.h>
-#include <Orbit.h>
 #include <Timer.h>
-#include <PID.h>
-#include <Common.h>
-#include <Vector.h>
-#include <Role.h>
-#include <MoveData.h>
-#include <Debug.h>
-#include <Defines.h>
 #include <Pins.h>
-#include <t3spi.h>
-#include <Lidar.h>
+#include <Defines.h>
 
-LIDAR lidars;
-
-Compass comp;
-
-CameraController camera;
-
-MotorController motors;
-
-LightSensorController lights;
-LightGate gate;
-
-Orbit orbit;
-
-Role role;
-
-// RoleController RC;
-
-MoveData move;
-
-T3SPI spi;
-
-Vector vector = Vector(0,0);
-
-bool inCorner;
-
-int lightVector;
-volatile uint16_t dataOut[1], dataIn[1];
-
-uint16_t transaction(uint8_t command, uint16_t data = 0){
-  dataOut[0] = (command << 14) | (data & 0x3FFF);
-
-  spi.txrx16(dataOut, dataIn, 1, CTAR_0, MASTER_CS_LIGHT);
-
-  return dataIn[0];
-}
+Timer kickInterval = Timer(KICK_INTERVAL);
+Timer kickLow = Timer(KICK_HIGH_TIME);
 
 void setup() {
-  pinMode(TEENSY_LED, OUTPUT);
-  #if DEBUG_ANY
-    Serial.begin(38400);
-  #endif
-  camera.setup();
-
-  digitalWrite(TEENSY_LED, HIGH);
-
-  Wire.begin();
-  comp.compassSetup();
-  comp.calibrate();
-
-  motors.motorSetup();
-  motors.brake();
-
-  orbit.setup();
-  orbit.resetAllData();
-
-  lidars.setup();
-
-  role = Role::defend;
-
-  digitalWrite(TEENSY_LED,LOW);
+  pinMode(KICKER_PIN, OUTPUT);
+  digitalWrite(KICKER_PIN, LOW);
 }
 
 void loop() {
-  // Compass
-  comp.updateGyro();
-  int heading = comp.getHeading();
-
-  // Camera
-  camera.update();
-
-  // Lidars
-  lidars.update();
-
-  // Orbit
-  orbit.setRole(role);
-  orbit.setGoalData(camera.getAttackGoal(), camera.getDefendGoal());
-  orbit.setBallData(camera.getBall());
-  orbit.setCompAngle(heading);
-  orbit.setCoords(lidars.getCoords());
-
-  // More Orbit
-  orbit.calculateMoveData();
-  orbit.calculateRotation();
-
-  // Movement
-  move = orbit.getMoveData();
-  motors.moveDirection(move);
-  // Serial.print(move.angle);
-  // Serial.print("\t");
-  // Serial.println(move.speed);
-
-  // End Loop
-  orbit.resetAllData();
+  if(kickInterval.hasTimePassed()){
+    digitalWrite(KICKER_PIN, HIGH);
+    kickLow.update();
+  }
+  if(kickLow.hasTimePassed()){
+    digitalWrite(KICKER_PIN, LOW);
+  }
 }
